2cd0449dd51dcc896244a12dd78e1bd79e879a7f
shdown <shdownnine@gmail.com>
2020-01-15 05:52:15 +0300

    SFDSF

--- tmp/packed.js
+++ tmp/packed.js
@@ -782,7 +782,7 @@ const createInput = params => {
 
 const createButton = params => {
   const btnKind = params.kind || 'primary';
-  const btn = (0, _utils.fromHtml)(`<button class="btn btn-${btnKind} btn-sm mr-3"></button>`);
+  const btn = (0, _utils.fromHtml)(`<button class="btn btn-${btnKind} btn-sm mr-1"></button>`);
   btn.append(params.value);
   if (params.type !== undefined) btn.setAttribute('type', params.type);
   if (params.onclick !== undefined) btn.onclick = params.onclick;
@@ -824,7 +824,7 @@ class FormView extends _view.View {
       this._getSubsBtn = createButton({
         group: inputGroup,
         value: (0, _gettext.__)('Fill with user subscriptions'),
-        kind: 'outline-primary',
+        kind: 'primary',
         block: true,
         onclick: () => {
           super._emitSignal('get-subs');
@@ -854,14 +854,14 @@ class FormView extends _view.View {
       const inputGroup = createInputGroup();
       this._submitBtn = createButton({
         group: inputGroup,
-        kind: 'outline-primary',
+        kind: 'primary',
         value: (0, _gettext.__)('Find!'),
         type: 'submit'
       });
       this._archiveBtn = createButton({
         group: inputGroup,
         value: (0, _gettext.__)('Archive'),
-        kind: 'outline-secondary',
+        kind: 'secondary',
         onclick: () => {
           super._emitSignal('open-archive');
 
